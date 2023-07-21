e386cb990917051a196b2f612b7aa4ca56d0966d
wisarmy <wisarmy@gmail.com>
2023-07-21 12:01:45 +0800

    remove toml

--- exbot-py/config.py
+++ exbot-py/config.py
@@ -1,4 +1,4 @@
-import toml
+import tomllib
 from pydantic import BaseModel
 
 class Exchange(BaseModel):
@@ -13,5 +13,6 @@ class Config:
         self.exchange = Exchange(**data['exchange'])
 
 def load_config(config_path) -> Config:
-    config = toml.load(config_path)
-    return Config(config)
+    with open(config_path, 'rb') as f:
+        config = tomllib.load(f)
+        return Config(config)
