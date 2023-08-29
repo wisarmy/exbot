31b850ae639d69352b12f5881dbd785d4c51da12
wisarmy <wisarmy@gmail.com>
2023-08-29 16:56:57 +0800

    update strategy

--- exbot-py/strategies/strategy.py
+++ exbot-py/strategies/strategy.py
@@ -7,6 +7,7 @@ def amount_limit(ex: BitgetExchange, df, symbol, amount, amount_max_limit):
     last_date = df.index[-1]
     # 获取当前仓位
     position = ex.fetch_position(symbol)
+    print(f"position: {position}")
 
     side = 'buy' if last['buy'] == 1 else 'sell' if last['sell'] == 1 else None
     if side is None:
@@ -32,6 +33,11 @@ def amount_limit(ex: BitgetExchange, df, symbol, amount, amount_max_limit):
             else:
                 # 超出最大仓位
                 print(f"long position is max: {position['long']['qty']}")
+                # 如果是盈利的，平仓
+                if position['long']['upnl'] > 0:
+                    print(f"close long: {last['close']}, profit: {position['long']['upnl']}")
+                    ex.close_position(symbol, 'buy', position['long']['qty'])
+
 
     elif side == 'sell':
         # 判断是否有多仓
@@ -48,4 +54,8 @@ def amount_limit(ex: BitgetExchange, df, symbol, amount, amount_max_limit):
             else:
                 # 超出最大仓位
                 print(f"short position is max: {position['short']['qty']}")
+                # 如果是盈利的，平仓
+                if position['short']['upnl'] > 0:
+                    print(f"close short: {last['close']}, profit: {position['short']['upnl']}")
+                    ex.close_position(symbol, 'sell', position['short']['qty'])
     return side
