35696c3e995963b518cb72c656a74dd32ba6dacb
wisarmy <wisarmy@gmail.com>
2023-09-05 16:02:10 +0800

    strategy: fix typo

--- exbot-py/strategies/strategy.py
+++ exbot-py/strategies/strategy.py
@@ -135,8 +135,8 @@ def handle_stop_loss_fix_upnl(last, ex: BitgetExchange, symbol, position):
     return False
 
 
-def handle_take_profit_fix_urate(last, ex: BitgetExchange, symbol, position):
-    fix_urate = float(os.getenv("TAKE_PROFIT_FIX_URATE", 0))
+def handle_take_profit_fix_price_urate(last, ex: BitgetExchange, symbol, position):
+    fix_urate = float(os.getenv("TAKE_PROFIT_FIX_PRICE_URATE", 0))
     input_amount = float(os.getenv("CLOSE_AMOUNT", 0))
     if fix_urate > 0:
         for side in ["short", "long"]:
@@ -154,14 +154,14 @@ def handle_take_profit_fix_urate(last, ex: BitgetExchange, symbol, position):
                     else price * (1 - fix_urate)
                 )
                 logger.info(
-                    f"# take_profit_fix_urate {side}: close_price: {close_price}, close_amount: {close_amount}"
+                    f"# take_profit_fix_price_urate {side}: close_price: {close_price}, close_amount: {close_amount}"
                 )
                 if (side == "long" and close_price > price) or (
                     side == "short" and close_price < price
                 ):
                     profit = upnl * (close_amount / position_amount)
                     logger.info(
-                        f"take_profit_fix_urate {side}: {price}, amount: [{close_amount}/{position_amount}], profit: [{profit}/{upnl}], urate: {fix_urate}"
+                        f"take_profit_fix_price_urate {side}: {price}, amount: [{close_amount}/{position_amount}], profit: [{profit}/{upnl}], urate: {fix_urate}"
                     )
                     order_side = "buy" if side == "short" else "sell"
                     ex.close_position(symbol, order_side, close_amount)
@@ -169,8 +169,8 @@ def handle_take_profit_fix_urate(last, ex: BitgetExchange, symbol, position):
     return False
 
 
-def handle_stop_loss_fix_urate(last, ex: BitgetExchange, symbol, position):
-    fix_urate = float(os.getenv("STOP_LOSS_FIX_URATE", 0))
+def handle_stop_loss_fix_price_urate(last, ex: BitgetExchange, symbol, position):
+    fix_urate = float(os.getenv("STOP_LOSS_FIX_PRICE_URATE", 0))
     input_amount = float(os.getenv("CLOSE_AMOUNT", 0))
     if fix_urate > 0:
         for side in ["short", "long"]:
@@ -188,14 +188,14 @@ def handle_stop_loss_fix_urate(last, ex: BitgetExchange, symbol, position):
                     else price * (1 - fix_urate)
                 )
                 logger.info(
-                    f"# stop_loss_fix_urate {side}: close_price: {close_price}, close_amount: {close_amount}"
+                    f"# stop_loss_fix_price_urate {side}: close_price: {close_price}, close_amount: {close_amount}"
                 )
                 if (side == "long" and close_price < price) or (
                     side == "short" and close_price > price
                 ):
                     profit = upnl * (close_amount / position_amount)
                     logger.info(
-                        f"stop_loss_fix_urate {side}: {price}, amount: [{close_amount}/{position_amount}], profit: [{profit}/{upnl}], urate: {fix_urate}"
+                        f"stop_loss_fix_price_urate {side}: {price}, amount: [{close_amount}/{position_amount}], profit: [{profit}/{upnl}], urate: {fix_urate}"
                     )
                     order_side = "buy" if side == "short" else "sell"
                     ex.close_position(symbol, order_side, close_amount)
@@ -229,14 +229,14 @@ def amount_limit(ex: BitgetExchange, df, symbol, amount, amount_max_limit):
             set_used_cache(last_date, 1)
         elif handle_take_profit_fix_upnl(last, ex, symbol, position):
             set_used_cache(last_date, 1)
-        elif handle_take_profit_fix_urate(last, ex, symbol, position):
+        elif handle_take_profit_fix_price_urate(last, ex, symbol, position):
             set_used_cache(last_date, 1)
 
         if handle_stop_loss(last, ex, symbol, position):
             set_used_cache(last_date, 1)
         elif handle_stop_loss_fix_upnl(last, ex, symbol, position):
             set_used_cache(last_date, 1)
-        elif handle_stop_loss_fix_urate(last, ex, symbol, position):
+        elif handle_stop_loss_fix_price_urate(last, ex, symbol, position):
             set_used_cache(last_date, 1)
 
         return side
