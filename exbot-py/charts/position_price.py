bd3b9aabd4701282ac8126a5b9f411f34b67fe69
wisarmy <wisarmy@gmail.com>
2023-09-06 10:26:29 +0800

    chart: add scripts

--- exbot-py/charts/position_price.py
+++ exbot-py/charts/position_price.py
@@ -1,21 +1,16 @@
 import argparse
 import numpy as np
 import pandas as pd
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Output, Input
 import plotly.graph_objects as go
-
-data_update_interval = 10
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="exbot backtesting for python")
-    parser.add_argument(
-        "--symbol", type=str, required=True, help="The trading symbol to use"
-    )
     # add arg verbose
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
     args = parser.parse_args()
 
-    df = pd.read_csv(
+    df_symbols = pd.read_csv(
         "data/position.csv",
         names=[
             "date",
@@ -33,65 +28,89 @@ if __name__ == "__main__":
         ],
         index_col=0,
     )
-    df = df[df["symbol"].str.strip() == args.symbol]
-
-    print(df)
-
-    fig = go.Figure()
-    fig.layout.height = 760
-    fig.layout.dragmode = "pan"
-
-    take_profit_urate = 0.00786
-    stop_loss_urate = 0.00382
-    # short
-    df["short_take_profit_close_price"] = df["short_entry_price"] * (
-        1 - take_profit_urate
-    )
-    df["short_stop_loss_close_price"] = df["short_entry_price"] * (1 + stop_loss_urate)
-    # long
-    df["long_take_profit_close_price"] = df["long_entry_price"] * (
-        1 + take_profit_urate
-    )
-    df["long_stop_loss_close_price"] = df["long_entry_price"] * (1 - stop_loss_urate)
-
-    df["take_profit_close_price"] = np.where(
-        df["short_qty"] > 0,
-        df["short_take_profit_close_price"],
-        df["long_take_profit_close_price"],
-    )
-    df["stop_loss_close_price"] = np.where(
-        df["short_qty"] > 0,
-        df["short_stop_loss_close_price"],
-        df["long_stop_loss_close_price"],
-    )
-    for name, group in df.groupby("symbol"):
-        fig.add_trace(
-            go.Scatter(
-                x=df.index,
-                y=df["price"],
-                mode="lines",
-                line=dict(dash="dash", width=1),
-                name=name + "_price",
-            )
-        )
-
-        fig.add_trace(
-            go.Scatter(
-                x=group.index,
-                y=group["take_profit_close_price"],
-                mode="lines",
-                name=name + "_take_profit_close_price",
-            )
-        )
-        fig.add_trace(
-            go.Scatter(
-                x=group.index,
-                y=group["stop_loss_close_price"],
-                mode="lines",
-                name=name + "_stop_loss_close_price",
-            )
-        )
+    symbols = df_symbols["symbol"].str.strip().unique()
     app = Dash()
-    app.layout = html.Div([dcc.Graph(figure=fig)])
+    app.layout = html.Div(
+        [
+            dcc.Dropdown(
+                id="symbol-dropdown",
+                options=[{"label": s, "value": s} for s in symbols],
+                value="NEAR/USDT:USDT",
+            ),
+            dcc.Graph(
+                id="graph",
+                config={
+                    "scrollZoom": True,  # 启用或禁用滚动缩放
+                },
+            ),
+        ]
+    )
+
+    @app.callback(Output("graph", "figure"), Input("symbol-dropdown", "value"))
+    def update_graph(selected_symbol):
+        global df_symbols
+        df = df_symbols[df_symbols["symbol"].str.strip() == selected_symbol].copy()
+
+        fig = go.Figure()
+        fig.layout.height = 700
+        fig.layout.dragmode = "pan"
+
+        take_profit_urate = 0.00786
+        stop_loss_urate = 0.00382
+        # short
+        df["short_take_profit_close_price"] = df["short_entry_price"] * (
+            1 - take_profit_urate
+        )
+        df["short_stop_loss_close_price"] = df["short_entry_price"] * (
+            1 + stop_loss_urate
+        )
+        # long
+        df["long_take_profit_close_price"] = df["long_entry_price"] * (
+            1 + take_profit_urate
+        )
+        df["long_stop_loss_close_price"] = df["long_entry_price"] * (
+            1 - stop_loss_urate
+        )
+
+        df["take_profit_close_price"] = np.where(
+            df["short_qty"] > 0,
+            df["short_take_profit_close_price"],
+            df["long_take_profit_close_price"],
+        )
+        df["stop_loss_close_price"] = np.where(
+            df["short_qty"] > 0,
+            df["short_stop_loss_close_price"],
+            df["long_stop_loss_close_price"],
+        )
+        for name, group in df.groupby("symbol"):
+            fig.add_trace(
+                go.Scatter(
+                    x=df.index,
+                    y=df["price"],
+                    mode="lines",
+                    name=name + "_price",
+                )
+            )
+
+            fig.add_trace(
+                go.Scatter(
+                    x=group.index,
+                    y=group["take_profit_close_price"],
+                    mode="lines",
+                    line=dict(dash="dash", width=1),
+                    name=name + "_take_profit_close_price",
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=group.index,
+                    y=group["stop_loss_close_price"],
+                    mode="lines",
+                    line=dict(dash="dash", width=1),
+                    name=name + "_stop_loss_close_price",
+                )
+            )
+
+        return fig
 
     app.run_server(debug=True, use_reloader=False)
